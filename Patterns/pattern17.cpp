05cd2275003cedc7fdc034a8ad0120f3afaa2cfb
Dharuv Verma <dharuvverma210800@gmail.com>
2021-08-05 18:42:07 +0530

    pattern17.cpp

---
+++ Patterns/pattern17.cpp
@@ -0,0 +1,40 @@
+/*
+ *	Code to print the following pattern for n = 4
+ *	*     *
+ *	 *   *
+ *	  * *
+ *	   *
+ *	This code belongs to @DharuvVerma21	
+ */
+#include<iostream>
+using namespace std;
+int main(){
+	// n = number of rows
+	int n;
+	cin >> n;
+	// i = counter for number of rows
+	int i = 1;
+	while(i <= n){
+		// j = counter for number of columns
+		int j = 1;
+		// for spaces
+		while(j <= (i-1)){
+			cout << " ";
+			j++;
+		}
+		j = 1;
+		// for bordering stars
+		while(j <= (2*(n-i+1)-1)){
+			if (j == 1 || j == (2*(n-i+1)-1)){
+				cout << "*";
+			}
+			else{
+				cout << " ";
+			}
+			j++;
+		}
+		cout << endl;
+		i++;
+	}
+	return 0;
+}
