b4b5195c677de4df705be1ac0c64030fd776423c
Dharuv Verma <dharuvverma210800@gmail.com>
2021-08-05 18:53:40 +0530

    pattern19.cpp

---
+++ Patterns/pattern19.cpp
@@ -0,0 +1,52 @@
+/*
+ *	Code to print the following pattern for n = 4
+ *	   *
+ *	  **
+ *	 ***
+ *	****
+ *	***
+ *	**
+ *	*
+ *	This code belongs to @DharuvVerma21	
+ */
+#include<iostream>
+using namespace std;
+int main(){
+	// n = number of rows
+	int n;
+	cin >> n;
+	// i = counter for number of rows
+	int i = 1;
+	// for upper part
+	while(i <= n){
+		// j = counter for number of columns
+		int j = 1;
+		// for spaces
+		while(j <= (n-i)){
+			cout << " ";
+			j++;
+		}
+		j = 1;
+		// for bordering stars
+		while(j <= i){
+			cout << "*";
+			j++;
+		}
+		cout << endl;
+		i++;
+	}
+	i = 1;
+	n = n - 1;
+	// for lower half
+	while(i <= n){
+		// j = counter for number of columns
+		int j = 1;
+		while(j <= (n-i+1)){
+			cout << "*";
+			j++;
+		}
+		cout << "\n";
+		i++;
+	}
+	return 0;
+}
