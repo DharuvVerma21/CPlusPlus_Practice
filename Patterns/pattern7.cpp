fbe5d9665164a548b310bad58c287afe9df2ca19
Dharuv Verma <dharuvverma210800@gmail.com>
2021-08-05 16:26:49 +0530

    pattern7.cpp

---
+++ Patterns/pattern7.cpp
@@ -0,0 +1,35 @@
+/*
+ *	Code to print the following pattern for n = 4
+ *	   *
+ *	  **
+ *	 ***
+ *	**** 
+ *	This code belongs to @DharuvVerma21	
+ */
+#include<iostream>
+using namespace std;
+int main(){
+	// n = number of rows
+    int n;
+	cin >> n;
+	// i = counter for number of rows
+	int i = 1;
+	while(i <= n){
+		// j = counter for number of columns
+		// for spaces
+		int j = 1;
+		while(j <= (n-i)){
+			cout << " ";
+			j++;
+		}
+		// for stars
+		j = 1;
+		while(j <= i){
+			cout << "*";
+			j++;
+		}
+		cout << "\n";
+		i++;
+	}
+	return 0;
+}
