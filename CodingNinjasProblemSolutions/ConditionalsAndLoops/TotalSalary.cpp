60af1dff20a5c2583552fe2229b8208f351e9350
Dharuv Verma <dharuvverma210800@gmail.com>
2021-08-09 23:42:20 +0530

    TotalSalary.cpp

---
+++ CodingNinjasProblemSolutions/ConditionalsAndLoops/TotalSalary.cpp
@@ -0,0 +1,37 @@
+// Write a program to calculate the total salary of a person. The user has to enter the basic salary (an integer) and the grade (an uppercase character), and depending upon which the total salary is calculated as -
+// totalSalary = basic + hra + da + allow – pf
+#include<iostream>
+using namespace std;
+int roundoff(float x){
+    int a = x;
+    float b = x-a;
+    b = b*100;
+    if (b < 50){
+        return a;
+    }
+    else{
+        return a+1;
+    }
+}
+int main(){
+    int basic;
+    char grade;
+    cin >> basic >> grade;
+    float hra = basic/5.0;
+    float da = basic/2.0;
+    float pf = basic*0.11;
+    int allow;
+    switch(grade){
+        case 65:
+            allow = 1700;
+            break;
+        case 66:
+            allow = 1500;
+            break;
+        default:
+            allow = 1300;
+    }
+    float total_sal = basic + hra + da + allow - pf;
+    cout << roundoff(total_sal);
+    return 0;
+}
