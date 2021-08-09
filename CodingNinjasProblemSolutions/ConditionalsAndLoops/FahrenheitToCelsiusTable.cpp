e03bd9e659ffb8a76371445ffa07f2a1d8698ec7
Dharuv Verma <dharuvverma210800@gmail.com>
2021-08-09 23:34:22 +0530

    FahrenheitToCelsiusTable.cpp

---
+++ CodingNinjasProblemSolutions/ConditionalsAndLoops/FahrenheitToCelsiusTable.cpp
@@ -0,0 +1,11 @@
+// Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E) and Step Size (W), you need to convert all Fahrenheit values from Start to End at the gap of W, into their corresponding Celsius values and print the table.
+#include<iostream>
+using namespace std;
+int main(){
+    int s, e, w;
+    cin >> s >> e >> w;
+    while (s <= e){
+        cout << s << "\t" << int((s - 32)*5/9.0) << endl;
+        s = s + w;
+    }
+}
