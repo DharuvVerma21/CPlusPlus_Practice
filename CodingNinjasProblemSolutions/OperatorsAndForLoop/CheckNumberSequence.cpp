93d8e99440a7267617e7dd667317fffe3ca8f80c
Dharuv Verma <dharuvverma210800@gmail.com>
2021-08-11 18:39:55 +0530

    CheckNumberSequence.cpp

---
+++ CodingNinjasProblemSolutions/OperatorsAndForLoop/CheckNumberSequence.cpp
@@ -0,0 +1,48 @@
+// You are given S, a sequence of n integers i.e. S = s1, s2, ..., sn. Compute if it is possible to split S into two parts : s1, s2, ..., si and si+1, si+2, ….., sn (0 <= i <= n) in such a way that the first part is strictly decreasing while the second is strictly increasing one.
+// Note : We say that x is strictly larger than y when x > y.
+// So, a strictly increasing sequence can be 1 4 8. However, 1 4 4 is NOT a strictly increasing sequence.
+// That is, in the sequence if numbers are decreasing, they can start increasing at one point. And once the sequence of numbers starts increasing, they cannot decrease at any point further.
+// Sequence made up of only increasing numbers or only decreasing numbers is a valid sequence. So, in both the cases, print true.
+// You just need to print true/false. No need to split the sequence.
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	int a, i = 1, b;
+	cin >> a;
+	bool isDecreasing = true, isIncreasing = false;
+	while(i < n){
+		cin >> b;
+		if (isDecreasing){
+			if (a == b){
+				cout << "false" << endl;
+                isDecreasing = false;
+		 		break;
+			}
+			else if (a < b){
+				isDecreasing = false;
+				isIncreasing = true;
+			}
+		}
+		else{
+			if (isIncreasing){
+				if (a == b){
+					cout << "false" << endl;
+                    isIncreasing = false;
+					break;
+				}
+				else if (a > b){
+					isIncreasing = false;
+					cout << "false" << endl;
+					break;
+				}
+			}
+		}
+		a = b;
+		i++;
+	}
+	if (isDecreasing || isIncreasing || (!isDecreasing && isIncreasing)){
+		cout << "true" << endl;
+	}
+}
