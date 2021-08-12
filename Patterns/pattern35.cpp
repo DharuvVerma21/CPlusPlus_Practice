1d16fe62a194a27cf2c601da987a2a7a39b9984d
Dharuv Verma <dharuvverma210800@gmail.com>
2021-08-13 00:40:26 +0530

    pattern35.cpp

---
+++ Patterns/pattern35.cpp
@@ -0,0 +1,28 @@
+/*
+ *	Code to print the following pattern for n = 4
+ *	1
+ *	12
+ *	123
+ *	1234
+ *	This code belongs to @DharuvVerma21	
+ */
+#include<iostream>
+using namespace std;
+int main(){
+	// n = number of rows
+    int n;
+	cin >> n;
+	// i = counter for number of rows
+	int i = 1;
+	while(i <= n){
+		// j = counter for number of columns
+		int j = 1;
+		while(j <= i){
+			cout << j;
+			j++;
+		}
+		cout << "\n";
+		i++;
+	}
+	return 0;
+}
