daa52d38720079ba8473185953239d0bf7db3ff1
Dharuv Verma <dharuvverma210800@gmail.com>
2021-09-16 19:09:51 +0530

    Best Solution O(n) checkPermutation.cpp

--- CodingNinjasProblemSolutions/CharacterArrays&2DArrays/checkPermutation.cpp
+++ CodingNinjasProblemSolutions/CharacterArrays&2DArrays/checkPermutation.cpp
@@ -1,32 +1,17 @@
 // For a given two strings, 'str1' and 'str2', check whether they are a permutation of each other or not.
 #include<iostream>
-#include<cstring>
 #define max 1000001
 using namespace std;
-void selectionSort(char *string){
-   int start = 0;
-   while(string[start+1] != '\0'){
-      int index = start, min = string[start];
-      for (int i=start+1; string[i] != '\0'; i++){
-         if (min > string[i]){
-            min = string[i];
-            index = i;
-         }
-      }
-      char temp = string[index];
-      string[index] = string[start];
-      string[start] = temp;
-      start++;
-   }
-}
 bool stringPermutation(char *string1, char *string2){
-   if (strlen(string1) != strlen(string2)){
-      return false;
-   }
-   selectionSort(string1);
-   selectionSort(string2);
+   int freqArr[256] = {};
    for (int i=0; string1[i] != '\0'; i++){
-      if (string1[i] != string2[i]){
+      freqArr[string1[i]]++;
+   }
+   for (int i=0; string2[i] != '\0'; i++){
+      freqArr[string2[i]]--;
+   }
+   for (int i=0; i<256; i++){
+      if (freqArr[i] != 0){
          return false;
       }
    }
