f8d90beaf63f4190cafb8ed60fb8faae8518fd18
Dharuv Verma <dharuvverma210800@gmail.com>
2021-11-27 22:40:55 +0530

    sumOfDigitsRecursively.cpp

---
+++ CodingNinjasProblemSolutions/Recursion/sumOfDigitsRecursively.cpp
@@ -0,0 +1,16 @@
+// Write a recursive function that returns the sum of the digits of a given integer.
+
+#include <iostream>
+using namespace std;
+int sumOfDigits(int n) {
+    if (n == 0){
+        return 0;
+    }
+    return (n%10) + sumOfDigits(n/10);
+}
+
+int main() {
+   int n;
+   cin >> n;
+   cout << sumOfDigits(n) << endl;
+}
