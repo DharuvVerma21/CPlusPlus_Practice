8a240bbb46476f4b2d4647056b8b777aaf90f336
Dharuv Verma <dharuvverma210800@gmail.com>
2021-11-30 01:24:28 +0530

    allIndicesOfNumber.cpp

---
+++ CodingNinjasProblemSolutions/Recursion/allIndicesOfNumber.cpp
@@ -0,0 +1,35 @@
+// Given an array of length N and an integer x, you need to find all the indexes where x is present in the input array. Save all the indexes in an array (in increasing order).
+// Do this recursively. Indexing in the array starts from 0.
+
+#include<iostream>
+using namespace std;
+int helperFunction(int input[], int size, int x, int output[], int index){
+    if (size == 0){
+        return 0;
+    }
+    if (input[0] == x){
+        output[0] = index;
+        return 1 + helperFunction(input+1, size-1, x, output+1, index+1);
+    }
+    return helperFunction(input+1, size-1, x, output, index+1);
+}
+int allIndexes(int input[], int size, int x, int output[]) {
+    return helperFunction(input, size, x, output, 0);
+}
+int main(){
+    int n;
+    cin >> n;
+    int *input = new int[n];
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    int x;
+    cin >> x;
+    int *output = new int[n];
+    int size = allIndexes(input, n, x, output);
+    for(int i = 0; i < size; i++) {
+        cout << output[i] << " ";
+    }
+    delete [] input;
+    delete [] output;
+}
